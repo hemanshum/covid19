e1b428ec65169d2b43b8481c659cccaa17c6b4f6
Hemanshu Mahajan <hemanshu.mahajan@gmail.com>
2020-03-29 11:41:26 +0530

    Donation Button added

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,38 @@
-import React from "react";
-import ResourceList from "./ResourceList";
-// import Summary from "./Summary";
-import coronaImg from "../img/corona.png";
+import React from 'react';
+import ResourceList from './ResourceList';
+import Summary from './Summary';
+import coronaImg from '../img/corona.png';
+import DonateButton from './DonateButton';
 
 function App() {
-  return (
-    <div className="ui container spacer-vertical">
-      <h1 className="ui centered header">
-        <h2 className="ui centered header">
-          <img src={coronaImg} width="70" height="70" alt="img" />
-        </h2>
-        COVID19 INFO
-        {/* <div className="sub header">
-          Updates on Corona Virus Status in every Country
-        </div> */}
-      </h1>
-      <div className="ui divider"></div>
-      {/* <Summary /> */}
-      <div className="ui divider"></div>
-      <ResourceList />
-      <div className="ui divider"></div>
-      <h4 className="ui centered header">
-        Developed By
-        <div className="sub header">
-          Hemanshu Mahajan - Pune, Maharashtra. India
-        </div>
-        <div className="sub header">Email - hemanshu.mahajan175@gmail.com</div>
-        <div className="sub header">API provider - covid19.mathdro.id</div>
-      </h4>
-    </div>
-  );
+	return (
+		<div className="ui container spacer-vertical">
+			<h1 className="ui centered header">
+				<div className="ui centered header">
+					<img src={coronaImg} width="70" height="70" alt="img" />
+				</div>
+				COVID19 INFO
+				<div className="sub header">
+					Updates on Corona Virus Status in every Country
+				</div>
+				<div className="ui divider"></div>
+				<DonateButton />
+			</h1>
+			<div className="ui divider"></div>
+			<Summary />
+			<div className="ui divider"></div>
+			<ResourceList />
+			<div className="ui divider"></div>
+			<h4 className="ui centered header">
+				Developed By
+				<div className="sub header">
+					Hemanshu Mahajan - Pune, Maharashtra. India
+				</div>
+				<div className="sub header">Email - hemanshu.mahajan175@gmail.com</div>
+				<div className="sub header">API provider - covid19.mathdro.id</div>
+			</h4>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/DonateButton.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const DonateButton = () => {
+	return (
+		<div>
+			<a
+				href="https://www.pmindia.gov.in"
+				target="_blank"
+				rel="noopener noreferrer"
+				className="ui animated fade orange button big"
+			>
+				<div className="visible content">
+					<span role="img" aria-labelledby="jsx-a11y/accessible-emoji">
+						🇮🇳
+					</span>{' '}
+					Donate to PM CARES Account
+				</div>
+				<div className="hidden content">
+					<span role="img" aria-labelledby="jsx-a11y/accessible-emoji">
+						🙏
+					</span>{' '}
+					Ek choti rashi desh ke liye
+				</div>
+			</a>
+			<a
+				href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public"
+				target="_blank"
+				rel="noopener noreferrer"
+				className="ui animated fade orange basic button big"
+			>
+				<div className="visible content">
+					<span role="img" aria-labelledby="jsx-a11y/accessible-emoji">
+						👉🏻
+					</span>{' '}
+					Advice for the public
+				</div>
+				<div className="hidden content">Basic protective measures</div>
+			</a>
+		</div>
+	);
+};
+
+export default DonateButton;
